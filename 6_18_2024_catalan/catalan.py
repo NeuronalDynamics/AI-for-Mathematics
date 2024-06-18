2244d54ce8eae1a1330bfe2189a799c5fd510b5a
HMNeuronalDynamics <hanxuan@uw.com>
2024-06-18 10:48:32 -0700

    Update 06/18/2024

--- 6_18_2024_catalan/catalan.py
+++ 6_18_2024_catalan/catalan.py
@@ -17,8 +17,12 @@ def parenthesizations(n):
   if n == 0:
     return {""}
   else:
-    # TODO
-    pass
+    result = set()
+    for i in range(n):
+      for left in parenthesizations(i):
+        for right in parenthesizations(n - 1 - i):
+          result.add(f"({left}){right}")
+    return result
 
 def product_orders(n):
   """
@@ -41,8 +45,12 @@ def product_orders(n):
   elif n == 2:
     return {"?*?"}
   else:
-    # TODO
-    pass
+    result = set()
+    for i in range(1, n):
+      for left in product_orders(i):
+        for right in product_orders(n - i):
+          result.add(f"({left}*{right})")
+    return result
 
 def permutations_avoiding_231(n):
   """
@@ -61,8 +69,22 @@ def permutations_avoiding_231(n):
   if n < 3:
     return set(itertools.permutations(range(1, n+1)))
   else:
-    # TODO
-    pass
+    result = set()
+    for perm in itertools.permutations(range(1, n+1)):
+        valid = True
+        for i in range(n):
+            for j in range(i+1, n):
+                for k in range(j+1, n):
+                    if perm[i] < perm[k] < perm[j]:
+                        valid = False
+                        break
+                if not valid:
+                    break
+            if not valid:
+                break
+        if valid:
+            result.add(perm)
+    return result
 
 def triangulations(n):
   """
@@ -84,5 +106,12 @@ def triangulations(n):
   elif n == 3:
     return {tuple()}
   else:
-    pass
-    # TODO
+    result = set()
+    for k in range(1, n-1):
+        left_triangulations = triangulations(k+1)
+        right_triangulations = triangulations(n-k)
+        for left in left_triangulations:
+            for right in right_triangulations:
+                triangulation = tuple(sorted(list(left) + list(((0, k),)) + [(i + k, j + k) for i, j in right]))
+                result.add(triangulation)
+    return result
